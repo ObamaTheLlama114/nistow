1715ca28d3dd2855404470ae57e610be97f01aec
ObamaTheLlama114 <william.chomel@gmail.com>
2023-05-09 04:30:43 -0500

    more testing

--- src/server/kv.ts
+++ src/server/kv.ts
@@ -1,12 +1,13 @@
 import { server$ } from '@builder.io/qwik-city';
-import kv from '@vercel/kv';
 
 export const getValue = server$(async (key: string): Promise<string> => {
+	const kv = require('@vercel/kv');
 	console.log(process.env);
 	return await kv.get(key) || "";
 })
 
 export const setValue = server$(async (key: string, value: string) => {
+	const kv = require('@vercel/kv');
 	if (value === "") {
 		await kv.del(key);
 	} else {
