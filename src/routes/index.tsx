e616a8e4619a3d409ee53fcd4f8ed5009c9e85ac
ObamaTheLlama114 <william.chomel@gmail.com>
2023-05-09 02:37:59 -0500

    testing server$ functions with vercel kv

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,55 @@
-import { component$ } from '@builder.io/qwik';
-import type { DocumentHead } from '@builder.io/qwik-city';
+import { component$, useSignal, useTask$ } from '@builder.io/qwik';
+import { DocumentHead, server$ } from '@builder.io/qwik-city';
+import kv from '@vercel/kv';
+
+function useKeyValue(initialKey: string, initialValue: string) {
+	const key = useSignal(initialKey);
+	const value = useSignal(initialValue);
+
+	const getValue = server$(async (key: string): Promise<string> => {
+		return await kv.get(key) || "";
+	})
+
+	const setValue = server$(async (key: string, value: string) => {
+		if (value === "") {
+			await kv.del(key);
+		} else {
+			await kv.set(key, value);
+		}
+	})
+
+	useTask$(async ({ track }) => {
+		track(() => key.value);
+		const newValue = await getValue(key.value);
+		value.value = newValue;
+	});
+
+	useTask$(async ({ track }) => {
+		track(() => value.value);
+		setValue(key.value, value.value);
+	});
+
+	return [key, value] as const;
+}
 
 export default component$(() => {
-  return (
-    <>
-    </>
-  );
+	const [key, value] = useKeyValue("", "");
+
+	return (
+		<>
+			<input type="text" bind:value={key} />
+			<br/>
+			<input type="text" bind:value={value} />
+		</>
+	);
 });
 
 export const head: DocumentHead = {
-  title: 'Welcome to Qwik',
-  meta: [
-    {
-      name: 'description',
-      content: 'Qwik site description',
-    },
-  ],
+	title: 'Qwik',
+	meta: [
+		{
+			name: 'description',
+			content: 'Qwik site description',
+		},
+	],
 };
